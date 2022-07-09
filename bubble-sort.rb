9dc68871b7a417f86cfde01999a73700c4369ddc
BillC424 <wcurran44@gmail.com>
2022-07-09 16:42:47 -0400

    Modified bubble_sort method to iterate through all elements in the array once while comparing each element to the element in the index to the right to determine if the element is higher. If the element is higher, the numbers swap positions in the array

---
+++ bubble-sort.rb
@@ -0,0 +1,11 @@
+def bubble_sort(array)
+    sort_array = array
+    array[0...-1].each_with_index do |number, index|
+      if array[index] > array[index + 1]
+        sort_array[index], sort_array[index + 1] = sort_array[index + 1], sort_array[index]
+      end  
+    end
+   sort_array 
+  end
+  
+  p bubble_sort([4,3,78,2,0,2])
