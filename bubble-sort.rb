0ce3674d0f30ecff0851ab11616f1b2340e2ba24
BillC424 <wcurran44@gmail.com>
2022-07-09 17:09:27 -0400

    Nested the enumerable from the last commit within an each enumerable so that the sort could run enough times to completely sort the array

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,10 +1,12 @@
 def bubble_sort(array)
     sort_array = array
-    array[0...-1].each_with_index do |number, index|
-      if array[index] > array[index + 1]
-        sort_array[index], sort_array[index + 1] = sort_array[index + 1], sort_array[index]
-      end  
-    end
+    array[0...-1].each do |number|
+      array[0...-1].each_with_index do |number, index|
+        if array[index] > array[index + 1]
+          sort_array[index], sort_array[index + 1] = sort_array[index + 1], sort_array[index]
+        end  
+      end
+   end 
    sort_array 
   end
   
